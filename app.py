7dec4936feeae19d7f3fa67aad53b6ac84468d2b
younghwangit <twofireontree213@gmail.com>
2024-01-30 11:35:34 +0900

    update: Table added

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 #-*-coding:utf-8 -*-
 
-import streamlit as st  
+import streamlit as st
+import seaborn as sns
+
+@st.cache_data
+def load_data():
+    df = sns.load_dataset("iris")
+    return df
 
 def main():
     st.title("Hello World on Streamlit.io")
+    iris = load_data()
+    st.table(iris)
 
 if __name__ == "__main__":
     main()
+
+#배포할 때 파이썬 버전은 3.11로
